04fe4fd9d900211d9de688786f9d0b78a3d2ccad
Adam De La Torre <adamdelatorre610@gmail.com>
2026-04-29 18:27:35 -0500

    Actualización del archivo main.py
    Se actualizo el archivo principal del proyecto para visualizar el formulario de estudiantes.

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
-from routes import students
-from database import engine, Base
-
-Base.metadata.create_all(bind=engine)
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-app.include_router(students.router)
+
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
+
+templates = Jinja2Templates(directory="frontend")
+
+@app.get("/")
+async def read_index(request: Request):
+    # En lugar de un diccionario complejo, usamos argumentos con nombre si es necesario
+    return templates.TemplateResponse(
+        request=request, 
+        name="index.html", 
+        context={} # Aquí puedes meter tus datos del CRUD más adelante
+    )
