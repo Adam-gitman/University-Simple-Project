add6df06cfcfa30b79d6b9a370bcfd7a51f3b619
Adam De La Torre <adamdelatorre610@gmail.com>
2026-05-01 23:54:30 -0500

    Cambio en el contexto de las rutas
    Se modernizó el contexto de diccionario en las rutas para que fuera compatible con la librería jinja2.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from routes import students, auth
@@ -20,14 +21,44 @@ app.include_router(students.router)
 app.include_router(auth.router)
 app.mount("/static", StaticFiles(directory="frontend"), name="static")
 
+templates = Jinja2Templates(directory="frontend")
+
 @app.get("/")
-def index():
-    return FileResponse("frontend/login.html")
+async def read_index(request: Request):
+    return templates.TemplateResponse(
+        request=request, 
+        name="auth/login/login.html", 
+        context={}
+    )
 
-@app.get("/auth.html")
-def auth_page():
-    return FileResponse("frontend/auth.html")
+@app.get("/auth")
+async def read_index(request: Request):
+    return templates.TemplateResponse(
+        request=request, 
+        name="auth/auth.html", 
+        context={}
+    )
 
-@app.get("/index.html")
-def home():
-    return FileResponse("frontend/index.html")
+@app.get("/form")
+async def read_index(request: Request):
+    return templates.TemplateResponse(
+        request=request, 
+        name="/form/index.html", 
+        context={}
+    )
+
+@app.get("/form/edit")
+async def read_index(request: Request):
+    return templates.TemplateResponse(
+        request=request, 
+        name="/form/edit/index.html", 
+        context={}
+    )
+
+@app.get("/view-list")
+async def read_view_list(request: Request):
+    return templates.TemplateResponse(
+        request=request, 
+        name="view-list/index.html", 
+        context={}
+        )
